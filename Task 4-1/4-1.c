aacb30c2e16af1501402ff732b2437f3998d3c10
Ilya <ilya@andrianov.ru>
2023-11-01 23:32:34 +0300

    Промежуточная стадия 4-1 после правок

--- Task 4-1/4-1.c
+++ Task 4-1/4-1.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdbool.h>
-#define sizeOfNumbers 20
+#define SIZE_OF_NUMBERS 31
 
 void decToBin(int number, int array[])
 {
     if (number < 0)
     {
-        number = (int)(pow(2, sizeOfNumbers) + number);
+        number = (int)(pow(2, SIZE_OF_NUMBERS) + number);
     }
 
-    for (int i = 0; i < sizeOfNumbers; i++)
+    for (int i = 0; i < SIZE_OF_NUMBERS; i++)
     {
-        array[sizeOfNumbers - 1 - i] = number & 1;
+        array[SIZE_OF_NUMBERS - 1 - i] = number & 1;
         number = number >> 1;
     }
 }
@@ -20,7 +20,7 @@ void decToBin(int number, int array[])
 void sum(int number1[], int number2[], int array[])
 {
     int extraBit = 0;
-    for (int i = sizeOfNumbers - 1; i >= 0; i--)
+    for (int i = SIZE_OF_NUMBERS - 1; i >= 0; i--)
     {
         array[i] = (number1[i] + number2[i] + extraBit ) % 2;
         extraBit = ((number1[i] + number2[i] + extraBit) >= 2) ? 1 : 0;
@@ -30,25 +30,12 @@ void sum(int number1[], int number2[], int array[])
 int binToDec(int numberArray[])
 {
     int number = 0;
-    int inverter = 0;
-    int multiplier = 1;
-    int increment = 0;
 
-    if (numberArray[0] == 1)
-    {
-        inverter = 1;
-        multiplier = -1;
-        increment = 1;
-    }
-
-    for (int i = 1; i < sizeOfNumbers; i++)
+    for (int i = 1; i < SIZE_OF_NUMBERS; i++)
     {
         number = number << 1;
-        number = number + ((numberArray[i] + inverter) % 2);
+        number = number + (numberArray[i] % 2);
     }
-
-    number = number + increment;
-    number = number * multiplier;
 
     return number;
 }
@@ -64,46 +51,48 @@ void printIntArrayOfNumber(int array[], int len)
 
 int main()
 {
-    int number1, number2;
-    int maxNumber = (int)pow(2, sizeOfNumbers - 1) + 1;
+    int number1 = 0;
+    int number2 = 0;
+
+    int maxNumber = (int)pow(2, SIZE_OF_NUMBERS - 1) + 1;
     printf("Введите два числа: \n");
     scanf("%d", &number1);
     if (number1 >= maxNumber)
     {
-        printf("Ошибка: меньше 2**20\n");
+        printf("Ошибка: больше 2**32\n");
         return -1;
     }
     scanf("%d", &number2);
     if (number2 >= maxNumber)
     {
-        printf("Ошибка: меньше 2**20\n");
+        printf("Ошибка: больше 2**32\n");
         return -1;
     }
 
     printf("\n");
 
-    int number1Array[sizeOfNumbers];
-    int number2Array[sizeOfNumbers];
+    int number1Array[SIZE_OF_NUMBERS];
+    int number2Array[SIZE_OF_NUMBERS];
 
     decToBin(number1, number1Array);
     decToBin(number2, number2Array);
 
     printf("Первое число в двоичном представлении:   ");
-    printIntArrayOfNumber(number1Array, sizeOfNumbers);
+    printIntArrayOfNumber(number1Array, SIZE_OF_NUMBERS);
     printf("Второе число в двоичном представлении:   ");
-    printIntArrayOfNumber(number2Array, sizeOfNumbers);
+    printIntArrayOfNumber(number2Array, SIZE_OF_NUMBERS);
 
     printf("\n");
 
-    int sumArray[sizeOfNumbers];
+    int sumArray[SIZE_OF_NUMBERS];
     sum(number1Array, number2Array, sumArray);
 
     printf("Сложение их в столбик:\n ");
-    printIntArrayOfNumber(number1Array, sizeOfNumbers);
+    printIntArrayOfNumber(number1Array, SIZE_OF_NUMBERS);
     printf("+\n ");
-    printIntArrayOfNumber(number2Array, sizeOfNumbers);
+    printIntArrayOfNumber(number2Array, SIZE_OF_NUMBERS);
     printf("=\n ");
-    printIntArrayOfNumber(sumArray, sizeOfNumbers);
+    printIntArrayOfNumber(sumArray, SIZE_OF_NUMBERS);
 
     printf("\n");
 
